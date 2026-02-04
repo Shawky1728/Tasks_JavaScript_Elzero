190d1bbaa9807973743990f71d11c396762819ae
Mahmoud shawky <mahmoud345139@gmail.com>
2026-02-04 16:33:25 +0200

    Assignment 5 Task 1

---
+++ Assignment 5/Task 1/main.js
@@ -0,0 +1,7 @@
+let userName = "Elzero";
+console.log(userName.charAt(0).toLowerCase()); // e
+console.log(userName.toLowerCase()[0]); // e
+console.log(userName.toLowerCase().substring(0,1)); // e
+console.log(userName.toLowerCase().substr(0,1)); // e
+console.log(userName.toLowerCase().slice(0,1)); // e
+console.log(userName.toLowerCase()[0].repeat(3)); // eee
