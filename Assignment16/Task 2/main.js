c5c636504e457d8acca2ed7ed2760e5021519f0f
Mahmoud shawky <121881335+Shawky1728@users.noreply.github.com>
2026-07-14 18:23:08 +0300

    Assignment 16 Task 2

---
+++ Assignment16/Task 2/main.js
@@ -0,0 +1,5 @@
+for(let i = 0; i < 10; i++) {
+  var myImage = document.getElementsByTagName("img")[i];
+  myImage.src = "https://elzero.org/wp-content/themes/elzero/imgs/logo.png";
+  myImage.alt = "Elzero Logo";
+}
