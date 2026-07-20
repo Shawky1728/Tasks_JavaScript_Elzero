6fa973485f209470f4fa7c4bddbb81c70270da54
Mahmoud shawky <121881335+Shawky1728@users.noreply.github.com>
2026-07-20 19:47:34 +0300

    Assignment 18 Task 3

---
+++ Assignment 18/Task 3/main.js
@@ -0,0 +1,22 @@
+let div = document.createElement("div");
+div.textContent = "10";
+document.body.appendChild(div);
+
+
+
+
+let id = setInterval(countDown, 1000);
+
+
+
+function countDown() {
+
+    let myDiv = document.querySelector("div");
+    myDiv.innerHTML -= 1;
+    if (myDiv.innerHTML === "0") {
+        myDiv.textContent = "Done";
+        clearInterval(id);
+    }
+}
+
+
