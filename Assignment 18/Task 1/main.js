b4a7f808a733b51691a51851697f6a2bec071b8c
Mahmoud shawky <121881335+Shawky1728@users.noreply.github.com>
2026-07-20 19:10:39 +0300

    Edit Assignment 18 Task 1

--- Assignment 18/Task 1/main.js
+++ Assignment 18/Task 1/main.js
@@ -7,6 +7,10 @@ inputArray.sort((a, b) => a - b);
 let start = Number(inputArray[0]);
 let end = Number(inputArray[1]);
 
+let div = document.createElement("div");
 for (let i = start; i <= end; i++) {
-    console.log(i);
+    let p = document.createElement("p");
+    p.textContent = i;
+    div.appendChild(p);
 }
+document.body.appendChild(div);
