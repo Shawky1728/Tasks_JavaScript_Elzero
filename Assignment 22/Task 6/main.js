8baeacffd3e8e0f28d7d538a53a3a15e142b6917
Mahmoud shawky <121881335+Shawky1728@users.noreply.github.com>
2026-07-27 13:48:45 +0300

    refactor assignment 22 challenge

--- Assignment 22/Task 6/main.js
+++ Assignment 22/Task 6/main.js
@@ -4,7 +4,7 @@ let url3 = 'https://elzero.org';
 let url4 = 'https://www.elzero.org';
 let url5 = 'https://www.elzero.org:8080/articles.php?id=100&cat=topics';
 
-let re = /(https?:\/\/(www.)?)?elzero.org(:\w+\/\w+\.\w+\?\w+\=\w+\&\w+\=\w+)?/ig; // Write Your Pattern Here
+let re = /(https?:\/\/(www\.)?)?elzero.org(:\d+\/\w+\.\w+\?\w+\=\w+\&\w+\=\w+)?/ig; // Write Your Pattern Here
 
 console.log(url1.match(re));
 console.log(url2.match(re));
